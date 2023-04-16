0774f26a5989ce23bf8074c3a97bfb37319a70bb
Nyx <necrownyx@outlook.com>
2023-04-16 15:11:50 +1000

    added template input box to the ui

---
+++ ui/index.js
@@ -0,0 +1,12 @@
+let timer;
+
+document.addEventListener('input', e => {
+  const el = e.target;
+  
+  if( el.matches('[data-color]') ) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      document.documentElement.style.setProperty(`--color-${el.dataset.color}`, el.value);
+    }, 100)
+  }
+})
