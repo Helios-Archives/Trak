9b54a7b72c5da07e6af88ccd30247ab3326a5031
Nyx <necrownyx@outlook.com>
2023-04-16 17:22:08 +1000

    updated the get functions to make it easyer to compare sites

--- src/tools/get.py
+++ src/tools/get.py
@@ -1,7 +1,10 @@
 import requests
+import webbrowser
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-existing_account = "http://twitch.tv/necrownyx"
-non_existing_account = "http://twitch.tv/654124085703249857"
+existing_account = "http://youtube.com/@necrownyx"
+non_existing_account = "http://youtube.com/@necrownyx123456789"
 
 r1 = requests.get(existing_account)
 r2 = requests.get(non_existing_account)
@@ -9,3 +12,19 @@ r2 = requests.get(non_existing_account)
 print(r1.text)
 for i in range(0, 100): print('.')
 print(r2.text)
+
+# save the output of this script to a file
+# and open it in a browser to see the difference
+
+with open('1.html', 'w') as f:
+    f.write(str(r1.text.encode('utf-8')))
+
+with open('2.html', 'w') as f:
+    f.write(str(r2.text.encode('utf-8')))
+
+# open the files in a browser to see the difference
+filename = os.path.abspath('1.html')
+webbrowser.open('file://' + filename)
+
+filename = os.path.abspath('2.html')
+webbrowser.open('file://' + filename)
